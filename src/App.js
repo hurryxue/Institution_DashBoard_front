8a1b1aab9b987be459ec2e6ec0f9c180be78e15e
harryx <haorunxue@gmail.com>
2024-04-17 18:11:43 -0700

    front page for institution report

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import BokehPlot from "./BokehPlot";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [detail, setDetail] = useState('');
+    const [institutionList, setInstitutionList] = useState([]);
+    const [plotData, setPlotData] = useState(null);
+    const [loadingerror,setLoadingerror] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('http://127.0.0.1:8000/getinstitutions'); // 替换为你的API URL
+                const idList = await response.json();
+
+                const institutions = []
+
+                for (const id of idList) {
+                    const response = await fetch(`http://127.0.0.1:8000/report/${id}`);
+                    const results = await response.json();
+                    institutions.push(results);
+                }
+                setInstitutionList(institutions)
+                console.log(institutions);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+                setInstitutionList(null); // 在出错时设置数据为null
+            }
+        };
+
+        fetchData();
+    }, []); // 空数组意味着这个效果只在组件首次渲染时运行
+
+
+
+    const handleButtonClick = async (fileId,description) => {
+        setDetail(description);
+        setLoadingerror(null);
+        setPlotData(null)
+        console.log('handleButtonClick')
+        try {
+            const response = await fetch(`http://127.0.0.1:8000/retrive/${fileId}`)
+            const data = await response.json();
+            console.log(data)
+            setPlotData(data)
+
+        }catch (error) {
+            console.error('Error fetching diagram:', error);
+            setLoadingerror("The file does not exist")
+        }
+    };
+
+    // Style object for buttons container
+    const buttonContainerStyle = {
+        display: 'flex',
+        flexDirection: 'column',  // Makes the buttons stack vertically
+        alignItems: 'flex-start', // Aligns buttons to the left
+    };
+
+    return (
+        <div>
+            <h1>Institution Reports</h1>
+            <div style={buttonContainerStyle}>
+                {institutionList.map((inst, index) => (
+                    <button key={index}
+                            onClick={() => handleButtonClick(inst.institution_file_id, inst.institution_info)}>
+                        {inst.insitution_name}
+                    </button>
+                ))}
+            </div>
+            <p>{detail}</p>
+            <div>
+                <h1>Submission Report</h1>
+                {loadingerror ? (
+                    <p>Error loading the plot: {loadingerror}</p>
+                ) : (
+                    plotData ? <BokehPlot data={plotData}/> : <p>Loading...</p>
+
+                )}
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
---
+++ src/BokehPlot.js
@@ -0,0 +1,39 @@
+// BokehPlot.js
+
+import React, {useEffect} from 'react';
+import * as Bokeh from '@bokeh/bokehjs';
+
+const BokehPlot = ({data}) => {
+    // useEffect(() => {
+    //     const script = document.createElement('script');
+    //     script.type = 'text/javascript';
+    //     script.async = true;
+    //     script.textContent = `
+    //   Bokeh.embed.embed_item(${data}, 'myplot');
+    // `;
+    //     document.body.appendChild(script);
+    //
+    //     return () => {
+    //         document.body.removeChild(script);
+    //     };
+    // }, [data]);
+    useEffect(() => {
+        if (data) {
+            try {
+                console.log('BokehPlot rend')
+                console.log(data)
+                Bokeh.embed.embed_item(data);
+            } catch (error) {
+                console.error('Error parsing JSON or embedding chart:', error);
+            }
+        }
+    }, [data]);
+
+
+    // return <div><p>{JSON.stringify(data, null, 2)}</p></div>;
+    return <div><p>this is a test</p>
+        <div id='myplot'></div>
+    </div>
+};
+
+export default BokehPlot;
